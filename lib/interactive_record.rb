d843fb5b98d48648ddad7bdf0c4a11219943526a
Sushi Chanrai <sushi@pathworks.co>
2018-08-25 03:28:11 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -71,8 +71,8 @@ class InteractiveRecord
 
 #find a row by the attribute hash passed into the method
   def self.find_by(attribute_hash)
-    value = attribute_hash.values.first
-    key = attribute_hash.keys.first.to_s
+    value = attribute_hash.values.first #Susan
+    key = attribute_hash.keys.first.to_s #name
     sql = "SELECT * FROM #{self.table_name} WHERE #{key} = '#{value}'"
     DB[:conn].execute(sql)
   end
